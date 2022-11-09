16cfb434bf862183d8e1b4cb00399e692656aeff
Nolan Meyer <meyer09@gmail.com>
2022-11-09 13:38:01 -0600

    update 901

--- 901. Online Stock Span.py
+++ 901. Online Stock Span.py
@@ -1,6 +1,7 @@
 class StockSpanner(object):
 
     def __init__(self):
+        self.data = []
         
 
     def next(self, price):
@@ -14,13 +15,44 @@ class StockSpanner(object):
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
 # param_1 = obj.next(price)
+'''
+Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
 
+The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backward) for which the stock price was less than or equal to today's price.
 
-testCases = [[2,7,11,15], [3,2,4], [3,3], [3,2,3]]
-target = [9, 6, 6, 6]
-testOutputs = [[0,1], [1,2], [0,1], [0,2]]
+    For example, if the price of a stock over the next 7 days were [100,80,60,70,60,75,85], then the stock spans would be [1,1,1,2,1,4,6].
 
-tester = Solution()
+Implement the StockSpanner class:
+
+    StockSpanner() Initializes the object of the class.
+    int next(int price) Returns the span of the stock's price given that today's price is price.
+
+-------------------------------------------
+
+Example 1
+Input
+["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+[[], [100], [80], [60], [70], [60], [75], [85]]
+Output
+[null, 1, 1, 1, 2, 1, 4, 6]
+
+Explanation
+StockSpanner stockSpanner = new StockSpanner();
+stockSpanner.next(100); // return 1
+stockSpanner.next(80);  // return 1
+stockSpanner.next(60);  // return 1
+stockSpanner.next(70);  // return 2
+stockSpanner.next(60);  // return 1
+stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
+stockSpanner.next(85);  // return 6
+'''
+
+testCase1 = ["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+testCase2 = [[], [100], [80], [60], [70], [60], [75], [85]]
+
+testOutputs = [null, 1, 1, 1, 2, 1, 4, 6]
+
+tester = StockSpanner()
 iter = 0
 for case in testCases:
     output = tester.twoSum(testCases[iter], target[iter])
