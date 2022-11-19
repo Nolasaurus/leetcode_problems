94d9a265d2dad5b08dac2ecb35e77eeb92d06cf5
Nolan Meyer <100235194+Nolasaurus@users.noreply.github.com>
2022-11-18 18:24:26 -0600

    new 1909 change to pop

--- 1909. Remove One Element to Make the Array Strictly Increasing.py
+++ 1909. Remove One Element to Make the Array Strictly Increasing.py
@@ -6,24 +6,35 @@ The array nums is strictly increasing if nums[i - 1] < nums[i] for each index (1
 
 class Solution:
     def canBeIncreasing(self, nums: list[int]) -> bool:
-        
         amt_decr = 0
-        for i in range(len(nums)-3):
-            print(nums[i], nums[i+1], nums[i+2])
-            if nums[i+1] <= nums[i]:
-                amt_decr += 1
-            
-            if nums[i+2] <= nums[i]:
-                amt_decr += 1
-            if nums[i+3] <= nums[i]:
-                amt_decr += 1
+        for i in range(1, len(nums)-1,1):
+            prev = nums[i-1]
+            next = nums[i+1]
 
-        if nums[-2] >= nums[-1]:
-            amt_decr +=1
+
+            if ((prev > nums[i] >= next)):
+                return False
+            if ((prev == nums[i] >= next)):
+                return False
+
+            squid = nums[i]
+            if i < len(nums)-2:
+                if next >= nums[i+2]:
+                    if ((prev < nums[i] > next)):
+                        amt_decr+=1
+                    if ((prev > nums[i] < next)):
+                        amt_decr+=1
             
-        if amt_decr <2:
-            return True
-        else: return False
+            if ((prev < nums[i] == next)):
+                amt_decr+=1
+            if ((prev == nums[i] < next)):
+                amt_decr+=1
+
+
+            if amt_decr > 1:
+                return False
+
+        else: return True
     
     ## Go through list of numbers
     ## count how many of the next two numbers (i+1, i+2) are less than or equal to i
@@ -31,6 +42,6 @@ class Solution:
     
 
 test = Solution()
-cases = [[1,2,10,5,7], [2,3,1,2], [1,1,1], [6,562,624,803,747,981,841]]
+cases = [[1,2,10,5,7], [2,3,1,2], [1,1,1], [6,562,624,803,747,981,841], [13,205,553,527,790,238]]
 for case in cases:
     print(test.canBeIncreasing(case))
