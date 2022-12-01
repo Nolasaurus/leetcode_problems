9d03a5dfbb5143b06a2e7ec408f7c56ff3707391
Nolan Meyer <100235194+Nolasaurus@users.noreply.github.com>
2022-11-30 18:48:04 -0600

    solution

---
+++ 1672. Richest Customer Wealth.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maximumWealth(self, accounts: list[list[int]]) -> int:
+        """You are given an m x n integer grid accounts where 
+        accounts[i][j] is the amount of money the ith customer has in the jth bank. 
+        Return the wealth that the richest customer has. A customer's wealth is the 
+        amount of money they have in all their bank accounts. The richest customer 
+        is the customer that has the maximum wealth.
+
+        m == accounts.length
+        n == accounts[i].length
+        1 <= m, n <= 50
+        1 <= accounts[i][j] <= 100
+        """
+        cmax = 0
+        for customer in accounts:
+            if sum(customer) > max:
+                cmax = max(cmax, sum(customer))
+
+        return cmax
+        
+tester = Solution()
+
+accounts = [[1,2,3],[3,2,1]]
+corr = [6]
+
+print("next case")
+print(tester.maximumWealth(accounts))
+
+"""
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+Explanation:
+1st customer has wealth = 1 + 2 + 3 = 6
+2nd customer has wealth = 3 + 2 + 1 = 6
+Both customers are considered the richest with a wealth of 6 each, so return 6.
+"""
